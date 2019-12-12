b2c298db0e0b8685907655635d9500c9deb5dab7
borg3s <jasilva1@stefanini.com>
2019-12-12 14:38:39 -0300

    solved syntoms bug

--- src/States/data.state.js
+++ src/States/data.state.js
@@ -26,63 +26,68 @@ const dataState = {
       sort: 'asc'
     },
     {
-      label: 'Ultima Visita ao dentista ',
-      field: 'lastVisit',
+      label: 'Ultima Visita ao dentista',
+      field: 'Ultima Visita ao dentista',
       sort: 'asc'
     },
     {
       label: 'Medo de dentista',
-      field: 'lastVisit',
+      field: 'Medo de dentista',
       sort: 'asc'
     },
     {
       label: 'Problema de saude',
-      field: 'lastVisit',
+      field: 'Problema de saude',
       sort: 'asc'
     },
     {
       label: 'detalhes de problema de saúde',
-      field: 'lastVisit',
+      field: 'detalhes de problema de saúde',
       sort: 'asc'
     },
     {
       label: 'toma medicamentos',
-      field: 'lastVisit',
+      field: 'toma medicamentos',
       sort: 'asc'
     },
     {
       label: 'toma medicamentos',
-      field: 'lastVisit',
+      field: 'toma medicamentos',
       sort: 'asc'
     },
     {
       label: 'detalhes dos medicamentos',
-      field: 'lastVisit',
+      field: 'detalhes dos medicamentos',
       sort: 'asc'
     },
     {
       label: 'tem alergia',
-      field: 'lastVisit',
+      field: 'tem alergia',
       sort: 'asc'
     },
     {
       label: 'detalhes da alergia',
-      field: 'lastVisit',
+      field: 'detalhes da alergia',
       sort: 'asc'
     },
     {
       label: 'tem diabetes',
-      field: 'lastVisit',
+      field: 'tem diabetes',
       sort: 'asc'
     },
     {
       label: 'Problemas de coracao',
-      field: 'lastVisit',
+      field: 'Problemas de coracao',
       sort: 'asc'
     },
     {
       label: 'esta grávida',
-      field: 'lastVisit',
+      field: 'esta grávida',
+      sort: 'asc'
+    },
+    {
+      label: 'Sintomas',
+      field: 'Sintomas',
       sort: 'asc'
     },
     
